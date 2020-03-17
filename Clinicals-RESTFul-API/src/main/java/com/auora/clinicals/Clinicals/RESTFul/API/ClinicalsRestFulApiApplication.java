7b5c445c0dfe51b6bdd6979ff54de0d525012979
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-03-17 22:31:42 +0530

    generate maven project and add connect the database for run successfully

---
+++ Clinicals-RESTFul-API/src/main/java/com/auora/clinicals/Clinicals/RESTFul/API/ClinicalsRestFulApiApplication.java
@@ -0,0 +1,13 @@
+package com.auora.clinicals.Clinicals.RESTFul.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicalsRestFulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicalsRestFulApiApplication.class, args);
+	}
+
+}
---
+++ Clinicals-RESTFul-API/src/test/java/com/auora/clinicals/Clinicals/RESTFul/API/ClinicalsRestFulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auora.clinicals.Clinicals.RESTFul.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicalsRestFulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
