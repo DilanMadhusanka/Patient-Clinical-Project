774c50c9303e882014b9ae7cc92cbbe4f511f058
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-03-18 22:33:48 +0530

    make logic for calculate bmi

--- Clinicals-RESTFul-API/src/main/java/com/auora/clinicals/Clinicals/RESTFul/API/controller/PatientController.java
+++ Clinicals-RESTFul-API/src/main/java/com/auora/clinicals/Clinicals/RESTFul/API/controller/PatientController.java
@@ -1,5 +1,6 @@
 package com.auora.clinicals.Clinicals.RESTFul.API.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,13 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.auora.clinicals.Clinicals.RESTFul.API.model.ClinicalData;
 import com.auora.clinicals.Clinicals.RESTFul.API.model.Patient;
 import com.auora.clinicals.Clinicals.RESTFul.API.repos.PatientRepository;
 
 @RestController
 @RequestMapping("/api")
 public class PatientController {
-	
+
 	@Autowired
 	private PatientRepository repository;
 
@@ -23,20 +25,35 @@ public class PatientController {
 	public List<Patient> getPatient() {
 		return repository.findAll();
 	}
-	
+
 	@RequestMapping(value = "/patients/{id}", method = RequestMethod.GET)
 	public Patient getPatient(@PathVariable("id") int id) {
 		return repository.findById(id).get();
 	}
-	
+
 	@RequestMapping(value = "/patients", method = RequestMethod.POST)
 	public Patient savePatient(@RequestBody Patient patient) {
 		return repository.save(patient);
 	}
-	
+
 	@RequestMapping(value = "/patients/analysis/{id}")
 	public Patient analysis(@PathVariable("id") int id) {
 		Patient patient = repository.findById(id).get();
-		return null;
+		List<ClinicalData> clinicalData = patient.getClinicalData();
+		ArrayList<ClinicalData> duplicateClinicalData = new ArrayList<>(clinicalData);
+		for (ClinicalData eachEntry : duplicateClinicalData) {
+			if (eachEntry.getComponentName().equals("hw")) {
+				String[] heightAndWeight = eachEntry.getComponentValue().split("/");
+				if (heightAndWeight != null && heightAndWeight.length > 1) {
+					float heightInMeter = Float.parseFloat(heightAndWeight[0]) * 0.4536F;
+					float bmi = Float.parseFloat(heightAndWeight[1]) / (heightInMeter);
+					ClinicalData bmiData = new ClinicalData();
+					bmiData.setComponentName("bmi");
+					bmiData.setComponentValue(Float.toString(bmi));
+					clinicalData.add(bmiData);
+				}
+			}
+		}
+		return patient;
 	}
 }
