de8ff8130ec28dfbb1cf3bbf65c8cc7f0519fb9c
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-03-19 22:47:07 +0530

    start implement Home Page get the patient details from backend using axios and a component life cycle method

--- clinicalsapp/src/components/Home.js
+++ clinicalsapp/src/components/Home.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import axios from 'axios';
 
 class Home extends React.Component {
-    render () {
+    state = {
+        patientData: []
+    }
+    componentWillMount() {
+        axios.get('localhost:8080/clinicalservices/api/patients').then(res => {
+            const patientData = res.data;
+            this.setState({ patientData })
+        })
+    }
+    render() {
         return (
-            <div></div>
+            <div>
+                <h2>Patient:</h2>
+                <table align='center'>
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Age</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.patientData.map(patient => <RowCreator item={patient} />)}
+                    </tbody>
+                </table>
+            </div>
         )
     }
 }
