a8c0a169695b44f64597f07d1a76f8c6c7bc53c0
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-03-20 09:39:05 +0530

    implement RowCreator

--- clinicalsapp/src/components/CollectClinicals.js
+++ clinicalsapp/src/components/CollectClinicals.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
 class CollectClinicals extends React.Component {
+    componentWillMount() {
+
+    }
     render () {
         return (
-            <div></div>
+            <div>
+                <h2>Patient Details:</h2>
+                First Name: {this.state.firstName}
+                Last Name: {this.state.lastName}
+                Age: {this.state.age}
+                <h2>ClinicalData:</h2>
+                <form>
+                    Clinical Entry Type <select>
+                        <option value="bp">Blood Pressure(Sys/Dys)</option>
+                        <option value="hw">Height/Weight</option>
+                        <option value="heartRate">Heart Rate</option>
+                    </select>
+                </form>
+            </div>
         )
     }
 }
--- clinicalsapp/src/components/Home.js
+++ clinicalsapp/src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
     state = {
@@ -28,7 +29,25 @@ class Home extends React.Component {
                         {this.state.patientData.map(patient => <RowCreator item={patient} />)}
                     </tbody>
                 </table>
+                <br/>
+                <Link to={'/addPatient'}><font size="5">Register Patient</font></Link>
             </div>
+        )
+    }
+}
+
+class RowCreator extends React.Component {
+    render() {
+        var patient = this.props.item;
+        return(
+            <tr>
+                <td>{patient.id}</td>
+                <td>{patient.firstName}</td>
+                <td>{patient.lastName}</td>
+                <td>{patient.age}</td>
+                <td><Link to={'/patientDetails/'+patient.id} >Add Data</Link></td>
+                <td><Link to={'/analyze/'+patient.id} >Analyze</Link></td>
+            </tr>
         )
     }
 }
