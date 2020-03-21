8727d49c15724bb4a5fd8a18ec3d09d02f554acb
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-03-21 18:08:05 +0530

    render clinicalsData using TableCreator class

--- clinicalsapp/src/components/ChartGenerator.js
+++ clinicalsapp/src/components/ChartGenerator.js
@@ -9,18 +9,32 @@ class ChartGenerator extends React.Component {
         axios.get("http://localhost:8080/clinicalservices/api/clinicals/" + this.props.match.params.patientId + "/" + this.props.match.params.componentName)
             .then(res => {
                 const clinicalsData = res.data
-                this.setState({clinicalsData})
+                this.setState({ clinicalsData })
             })
     }
     render() {
         return (
             <div>
-                
+                {this.state.clinicalsData.map(data => <TableCreator item={data} />)}
             </div>
         )
     }
 }
 
-
+class TableCreator extends React.Component {
+    render() {
+        return (
+            <div>
+                <table>
+                    <tr>
+                        <td>{this.props.item.id}</td>
+                        <td>{this.props.item.componentName}</td>
+                        <td>{this.props.item.componentValue}</td>
+                    </tr>
+                </table>
+            </div>
+        )
+    }
+}
 
 export default ChartGenerator;
