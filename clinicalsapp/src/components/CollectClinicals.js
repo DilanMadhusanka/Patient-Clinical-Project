79010205c66354f76f5383f68b66debe62bc7143
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-03-22 09:19:53 +0530

    add back button to AddPatient component

--- clinicalsapp/src/components/CollectClinicals.js
+++ clinicalsapp/src/components/CollectClinicals.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
+import Header from './Header';
+import Footer from './Footer'
+import { Link } from 'react-router-dom';
 
 class CollectClinicals extends React.Component {
     state = {}
@@ -27,21 +30,76 @@ class CollectClinicals extends React.Component {
     render() {
         return (
             <div>
-                <h2>Patient Details:</h2>
-                First Name: {this.state.firstName}
-                Last Name: {this.state.lastName}
-                Age: {this.state.age}
-                <h2>Patient Clinical Data:</h2>
-                <form>
-                    Clinical Entry Type <select onChange={(event) => { this.componentName = event.target.value }}>
-                        <option>Select One</option>
-                        <option value="bp">Blood Pressure(Sys/Dys)</option>
-                        <option value="hw">Height/Weight</option>
-                        <option value="heartRate">Heart Rate</option>
-                    </select>
-                    Value: <input type="text" name="componentValue" onChange={(event) => { this.componentValue = event.target.value }} />
-                    <button onClick={this.handleSubmit.bind(this)} >Confirm</button>
-                </form>
+                <Header />
+                <div className="ui container">
+                    <h2>Patient Details:</h2>
+                    <div class="ui text container">
+                        <div class="ui middle aligned selection list">
+                            <div class="item">
+                                <i class="chevron right icon"></i>
+                                <div class="content">
+                                    <div class="header">First Name: {this.state.firstName}</div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <i class="chevron right icon"></i>
+                                <div class="content">
+                                    <div class="header">Last Name: {this.state.lastName}</div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <i class="chevron right icon"></i>
+                                <div class="content">
+                                    <div class="header">Age: {this.state.age}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <h2>Patient Clinical Data:</h2>
+                    <div class="ui very padded segment">
+                        <p>
+                            <form className="ui form">
+                                <div class="ui inverted segment">
+                                    <div class="ui inverted form">
+                                        <div class="two fields">
+                                            <div class="field">
+                                                <div class="two fields">
+                                                    <div class="field">
+                                                        <label>State</label>
+                                                        <select class="ui fluid dropdown" onChange={(event) => { this.componentName = event.target.value }}>
+                                                            <option value="">Select One</option>
+                                                            <option value="bp">Blood Pressure(Sys/Dys)</option>
+                                                            <option value="hw">Height/Weight</option>
+                                                            <option value="heartrate">Heart Rate</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="field">
+                                                <label>Value</label>
+                                                <input placeholder="Value" type="text" name="componentValue" onChange={(event) => { this.componentValue = event.target.value }} />
+                                            </div>
+                                        </div>
+                                        <div class="inline field">
+                                            <div class="ui checkbox">
+                                                <input type="checkbox" tabindex="0" class="hidden" />
+                                                <label>I agree to the terms and conditions</label>
+                                            </div>
+                                        </div>
+                                        <button className="ui submit button" onClick={this.handleSubmit.bind(this)} >Confirm</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </p>
+                        <div className="ui center aligned container">
+                            <Link class="large ui red button" to="/">
+                                <i className="ui angle double left icon"/>
+                                Back
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+                <Footer />
             </div>
         )
     }
